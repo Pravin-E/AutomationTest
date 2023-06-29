78cbc9f52eaccdd96f03de794fc1611d755d2aae
Pravin-E <72814442+Pravin-E@users.noreply.github.com>
2023-06-29 11:54:52 +0530

    Add files via upload

---
+++ target/classes/ExtentReporterNG.java
@@ -0,0 +1,31 @@
+package resources;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG {
+	static ExtentReports extent;
+	
+	
+	public static ExtentReports getReporterObject()
+	{
+		 
+		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setDocumentTitle("Test Results");
+		reporter.config().setReportName("WebAutomation Test Results");
+		
+		
+		extent = new ExtentReports();
+		
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Pravin");
+
+		
+		return extent;
+	}
+	
+	
+	
+
+}
